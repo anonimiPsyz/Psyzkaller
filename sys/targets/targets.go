anonimiPsyz/Psyzkaller

commit ad38483b9a6960a990493f0c7f853ad55092b938

pkg/csource: use defines from sys/syscall.h on *bsd
Fixes #841

sys/targets/targets.go
@@ -176,6 +176,9 @@ var List = map[string]map[string]*Target{
 			PageSize:    4 << 10,
 			CFlags:      []string{"-m64"},
 			CrossCFlags: []string{"-m64", "-static"},
+			NeedSyscallDefine: func(uint64) bool {
+				return false
+			},
 		},
 	},
 	"netbsd": {
@@ -193,6 +196,9 @@ var List = map[string]map[string]*Target{
 			CFlags:      []string{"-m64"},
 			CCompiler:   "c++",
 			CrossCFlags: []string{"-m64", "-static", "-lutil"},
+			NeedSyscallDefine: func(uint64) bool {
+				return false
+			},
 		},
 	},
 	"fuchsia": {

executor/common_bsd.h
@@ -8,6 +8,7 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/syscall.h>
 
 #if GOOS_openbsd
 

pkg/csource/generated.go
@@ -405,6 +405,7 @@ void child()
 #include <stdarg.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/syscall.h>
 
 #if GOOS_openbsd
 

commit 1a0e16a29068b78c74137bbe5ccfc9fb5dfb130c

sys/targets: some syscalls on OpenBSD does need defines
Some syscalls on OpenBSD violates the ordinary SYS_ prefix convention. This is
an exhaustive enumeration of the deviations.

Regression introduced in commit ad38483b ("pkg/csource: use defines from
sys/syscall.h on *bsd").

sys/targets/targets.go
@@ -196,7 +196,29 @@ var List = map[string]map[string]*Target{
 			CFlags:      []string{"-m64"},
 			CCompiler:   "c++",
 			CrossCFlags: []string{"-m64", "-static", "-lutil"},
-			NeedSyscallDefine: func(uint64) bool {
+			NeedSyscallDefine: func(nr uint64) bool {
+				switch nr {
+				case 8: // SYS___tfork
+					return true
+				case 94: // SYS___thrsleep
+					return true
+				case 198: // SYS___syscall
+					return true
+				case 295: // SYS___semctl
+					return true
+				case 301: // SYS___thrwakeup
+					return true
+				case 302: // SYS___threxit
+					return true
+				case 303: // SYS___thrsigdivert
+					return true
+				case 304: // SYS___getcwd
+					return true
+				case 329: // SYS___set_tcb
+					return true
+				case 330: // SYS___get_tcb
+					return true
+				}
 				return false
 			},
 		},