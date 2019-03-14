anonimiPsyz/Psyzkaller

commit 432ba3f55a2c56cb150eeec4a11e24964ce528f3

pkg/build: reduce cyclomatic complexity of getBuilder()

pkg/build/build.go
@@ -63,22 +63,30 @@ type builder interface {
 }
 
 func getBuilder(targetOS, targetArch, vmType string) (builder, error) {
-	switch {
-	case targetOS == "linux" && targetArch == "amd64" && vmType == "gvisor":
-		return gvisor{}, nil
-	case targetOS == "linux" && targetArch == "amd64" && (vmType == "qemu" || vmType == "gce"):
-		return linux{}, nil
-	case targetOS == "fuchsia" && (targetArch == "amd64" || targetArch == "arm64") && vmType == "qemu":
-		return fuchsia{}, nil
-	case targetOS == "akaros" && targetArch == "amd64" && vmType == "qemu":
-		return akaros{}, nil
-	case targetOS == "openbsd" && targetArch == "amd64" && (vmType == "gce" || vmType == "vmm"):
-		return openbsd{}, nil
-	case targetOS == "netbsd" && targetArch == "amd64" && (vmType == "qemu" || vmType == "gce"):
-		return netbsd{}, nil
-	default:
-		return nil, fmt.Errorf("unsupported image type %v/%v/%v", targetOS, targetArch, vmType)
+	var supported = []struct {
+		OS   string
+		arch string
+		vms  []string
+		b    builder
+	}{
+		{"linux", "amd64", []string{"gvisor"}, gvisor{}},
+		{"linux", "amd64", []string{"gce", "qemu"}, linux{}},
+		{"fuchsia", "amd64", []string{"gce"}, fuchsia{}},
+		{"fuchsia", "arm64", []string{"gce"}, fuchsia{}},
+		{"akaros", "amd64", []string{"qemu"}, akaros{}},
+		{"openbsd", "amd64", []string{"gce", "vmm"}, openbsd{}},
+		{"netbsd", "amd64", []string{"gce", "qemu"}, netbsd{}},
 	}
+	for _, s := range supported {
+		if targetOS == s.OS && targetArch == s.arch {
+			for _, vm := range s.vms {
+				if vmType == vm {
+					return s.b, nil
+				}
+			}
+		}
+	}
+	return nil, fmt.Errorf("unsupported image type %v/%v/%v", targetOS, targetArch, vmType)
 }
 
 func CompilerIdentity(compiler string) (string, error) {

commit 75b19b54fac4797623a47a67d6f98611d884c5a9

pkg/build: fix getBuilder for fuchsia.
This CL changes the supported vmType for fuchsia to be QEMU, instead of
GCE.

This was broken by commit 432ba3f55, which changed the behavior of
getBuilder. It was part of a 6-commit PR trying to add support for
FreeBSD.

That commit has caused syzkaller to stop being able to build the fuchsia
image, getting the following error:

`kernel build failed: unsupported image type fuchsia/amd64/qemu`

pkg/build/build.go
@@ -71,8 +71,8 @@ func getBuilder(targetOS, targetArch, vmType string) (builder, error) {
 	}{
 		{"linux", "amd64", []string{"gvisor"}, gvisor{}},
 		{"linux", "amd64", []string{"gce", "qemu"}, linux{}},
-		{"fuchsia", "amd64", []string{"gce"}, fuchsia{}},
-		{"fuchsia", "arm64", []string{"gce"}, fuchsia{}},
+		{"fuchsia", "amd64", []string{"qemu"}, fuchsia{}},
+		{"fuchsia", "arm64", []string{"qemu"}, fuchsia{}},
 		{"akaros", "amd64", []string{"qemu"}, akaros{}},
 		{"openbsd", "amd64", []string{"gce", "vmm"}, openbsd{}},
 		{"netbsd", "amd64", []string{"gce", "qemu"}, netbsd{}},