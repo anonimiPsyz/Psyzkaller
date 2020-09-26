m5imunovic/syzkaller

tools/syz-cover: add command line option for coverage export
Add new command line option for exporting coverage information
calculated form rawcover file input into CSV file. The columns of
the exported file are:
Filename - function source file path
Function - function name
Percent  - coverage percentage
PCs      - number of total program counters

tools/syz-cover/syz-cover.go
@@ -40,6 +40,7 @@ func main() {
 		flagKernelSrc      = flag.String("kernel_src", "", "path to kernel sources")
 		flagKernelBuildSrc = flag.String("kernel_build_src", "", "path to kernel image's build dir (optional)")
 		flagKernelObj      = flag.String("kernel_obj", "", "path to kernel build/obj dir")
+		flagExport         = flag.String("csv", "", "export coverage data in csv format (optional)")
 	)
 	flag.Parse()
 
@@ -72,8 +73,14 @@ func main() {
 	}
 	progs := []cover.Prog{{PCs: pcs}}
 	buf := new(bytes.Buffer)
-	if err := rg.Do(buf, progs); err != nil {
+	bufcsv := new(bytes.Buffer)
+	if err := rg.Do(buf, bufcsv, progs); err != nil {
 		failf("%v", err)
+	}
+	if flagExport != nil {
+		if err := osutil.WriteFile(*flagExport, bufcsv.Bytes()); err != nil {
+			failf("%v", err)
+		}
 	}
 	fn, err := osutil.TempFile("syz-cover")
 	if err != nil {

anonimiPsyz/Psyzkaller

Signed-off-by: Jouni Hogander <jouni.hogander@unikie.com>

tools/syz-cover/syz-cover.go
@@ -40,6 +40,7 @@ func main() {
 		flagKernelSrc      = flag.String("kernel_src", "", "path to kernel sources")
 		flagKernelBuildSrc = flag.String("kernel_build_src", "", "path to kernel image's build dir (optional)")
 		flagKernelObj      = flag.String("kernel_obj", "", "path to kernel build/obj dir")
+		flagExport         = flag.String("csv", "", "export coverage data in csv format (optional)")
 	)
 	flag.Parse()
 
@@ -72,6 +73,15 @@ func main() {
 	}
 	progs := []cover.Prog{{PCs: pcs}}
 	buf := new(bytes.Buffer)
+	if *flagExport != "" {
+		if err := rg.DoCSV(buf, progs); err != nil {
+			failf("%v", err)
+		}
+		if err := osutil.WriteFile(*flagExport, buf.Bytes()); err != nil {
+			failf("%v", err)
+		}
+		return
+	}
 	if err := rg.DoHTML(buf, progs); err != nil {
 		failf("%v", err)
 	}