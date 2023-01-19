a-nogikh/syzkaller

syz-ci: move build semaphore closer to builds
Currently it's held during the whole job processing, which can take too
long.

Adjust it so that it's only taken when we really begin to build the
kernel or syzkaller.

syz-ci/syz-ci.go
@@ -219,7 +219,7 @@ func main() {
 		case <-shutdownPending:
 		case <-updatePending:
 		}
-		kernelBuildSem <- struct{}{} // wait for all current builds
+		buildSem.Wait() // wait for all current builds
 		close(stop)
 		wg.Done()
 	}()

anonimiPsyz/Psyzkaller

syz-ci/syz-ci.go
@@ -219,7 +219,7 @@ func main() {
 		case <-shutdownPending:
 		case <-updatePending:
 		}
-		kernelBuildSem <- struct{}{} // wait for all current builds
+		buildSem.WaitAll() // wait for all current builds
 		close(stop)
 		wg.Done()
 	}()