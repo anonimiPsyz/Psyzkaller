a-nogikh/syzkaller

dashboard/app: infer bug subsystems from crashes
After each saved crash, invoke the new pkg/subsystem machinery to infer
the subsystem list. Use 5 crashes with biggest priority to base the
inference on.

dashboard/app/api.go
@@ -23,7 +23,7 @@ import (
 	"github.com/google/syzkaller/pkg/auth"
 	"github.com/google/syzkaller/pkg/email"
 	"github.com/google/syzkaller/pkg/hash"
-	subsystem "github.com/google/syzkaller/pkg/subsystem/legacy"
+	"github.com/google/syzkaller/pkg/subsystem/entity"
 	"github.com/google/syzkaller/sys/targets"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/v2"
@@ -751,16 +751,19 @@ func reportCrash(c context.Context, build *Build, req *dashapi.Crash) (*Bug, err
 	} else if len(req.ReproSyz) != 0 {
 		reproLevel = ReproLevelSyz
 	}
-	newSubsystems := []string{}
+	newSubsystems := []*entity.Subsystem{}
 	save := reproLevel != ReproLevelNone ||
 		bug.NumCrashes < int64(maxCrashes()) ||
 		now.Sub(bug.LastSavedCrash) > time.Hour ||
 		bug.NumCrashes%20 == 0 ||
 		!stringInList(bug.MergedTitles, req.Title)
 	if save {
-		newSubsystems = detectCrashSubsystems(c, req, build)
-		log.Infof(c, "determined subsystems: %q", newSubsystems)
 		if err := saveCrash(c, ns, req, bug, bugKey, build, assets); err != nil {
+			return nil, err
+		}
+		newSubsystems, err = inferSubsystems(c, bug, bugKey)
+		if err != nil {
+			log.Errorf(c, "%q: failed to extract subsystems: %s", bug.Title, err)
 			return nil, err
 		}
 	} else {
@@ -786,8 +789,8 @@ func reportCrash(c context.Context, build *Build, req *dashapi.Crash) (*Bug, err
 		if len(req.Report) != 0 {
 			bug.HasReport = true
 		}
-		for _, name := range newSubsystems {
-			bug.addSubsystem(BugSubsystem{name})
+		if len(newSubsystems) != 0 {
+			bug.SetSubsystems(newSubsystems)
 		}
 		bug.increaseCrashStats(now)
 		bug.HappenedOn = mergeString(bug.HappenedOn, build.Manager)
@@ -819,28 +822,6 @@ func parseCrashAssets(c context.Context, req *dashapi.Crash) ([]Asset, error) {
 		assets = append(assets, newAsset)
 	}
 	return assets, nil
-}
-
-const overrideSubsystemsKey = "set_subsystems"
-
-func detectCrashSubsystems(c context.Context, req *dashapi.Crash, build *Build) []string {
-	val, ok := c.Value(overrideSubsystemsKey).([]string)
-	if ok {
-		return val
-	}
-	if build.OS == targets.Linux {
-		extractor := subsystem.MakeLinuxSubsystemExtractor()
-		return extractor.Extract(&subsystem.Crash{
-			GuiltyFiles: req.GuiltyFiles,
-			SyzRepro:    string(req.ReproSyz),
-		})
-	}
-	return nil
-}
-
-func subsystemMaintainers(ns, subsystemName string) []string {
-	nsConfig := config.Namespaces[ns]
-	return nsConfig.Subsystems.SubsystemCc(subsystemName)
 }
 
 func (crash *Crash) UpdateReportingPriority(build *Build, bug *Bug) {

dashboard/app/app_test.go
@@ -15,7 +15,9 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/syzkaller/dashboard/dashapi"
 	"github.com/google/syzkaller/pkg/auth"
-	subsystem "github.com/google/syzkaller/pkg/subsystem/legacy"
+	"github.com/google/syzkaller/pkg/subsystem"
+	"github.com/google/syzkaller/pkg/subsystem/entity"
+	_ "github.com/google/syzkaller/pkg/subsystem/lists"
 	"github.com/google/syzkaller/sys/targets"
 	"google.golang.org/appengine/v2/user"
 )
@@ -103,6 +105,22 @@ var testConfig = &GlobalConfig{
 					DailyLimit: 3,
 					Config: &TestConfig{
 						Index: 2,
+					},
+				},
+			},
+			Subsystems: SubsystemsConfig{
+				List: []*entity.Subsystem{
+					{
+						Name:        "subsystemA",
+						PathRules:   []entity.PathRule{{IncludeRegexp: `a\.c`}},
+						Lists:       []string{"subsystemA@list.com"},
+						Maintainers: []string{"subsystemA@person.com"},
+					},
+					{
+						Name:        "subsystemB",
+						PathRules:   []entity.PathRule{{IncludeRegexp: `b\.c`}},
+						Lists:       []string{"subsystemB@list.com"},
+						Maintainers: []string{"subsystemB@person.com"},
 					},
 				},
 			},
@@ -358,7 +376,7 @@ var testConfig = &GlobalConfig{
 				},
 			},
 			Subsystems: SubsystemsConfig{
-				SubsystemCc: subsystem.LinuxGetMaintainers,
+				List: subsystem.GetList("linux"),
 			},
 		},
 		"test-decommission": {

dashboard/app/entities.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/google/syzkaller/dashboard/dashapi"
 	"github.com/google/syzkaller/pkg/hash"
+	"github.com/google/syzkaller/pkg/subsystem/entity"
 	"golang.org/x/net/context"
 	db "google.golang.org/appengine/v2/datastore"
 )
@@ -129,13 +130,11 @@ type BugSubsystem struct {
 	Name string
 }
 
-func (bug *Bug) addSubsystem(subsystem BugSubsystem) {
-	for _, item := range bug.Tags.Subsystems {
-		if item.Name == subsystem.Name {
-			return
-		}
+func (bug *Bug) SetSubsystems(list []*entity.Subsystem) {
+	bug.Tags.Subsystems = nil
+	for _, item := range list {
+		bug.Tags.Subsystems = append(bug.Tags.Subsystems, BugSubsystem{Name: item.Name})
 	}
-	bug.Tags.Subsystems = append(bug.Tags.Subsystems, subsystem)
 }
 
 func (bug *Bug) hasSubsystem(name string) bool {

anonimiPsyz/Psyzkaller

dashboard/app/api.go
@@ -23,7 +23,7 @@ import (
 	"github.com/google/syzkaller/pkg/auth"
 	"github.com/google/syzkaller/pkg/email"
 	"github.com/google/syzkaller/pkg/hash"
-	subsystem "github.com/google/syzkaller/pkg/subsystem/legacy"
+	"github.com/google/syzkaller/pkg/subsystem"
 	"github.com/google/syzkaller/sys/targets"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/v2"
@@ -751,21 +751,31 @@ func reportCrash(c context.Context, build *Build, req *dashapi.Crash) (*Bug, err
 	} else if len(req.ReproSyz) != 0 {
 		reproLevel = ReproLevelSyz
 	}
-	newSubsystems := []string{}
 	save := reproLevel != ReproLevelNone ||
 		bug.NumCrashes < int64(maxCrashes()) ||
 		now.Sub(bug.LastSavedCrash) > time.Hour ||
 		bug.NumCrashes%20 == 0 ||
 		!stringInList(bug.MergedTitles, req.Title)
 	if save {
-		newSubsystems = detectCrashSubsystems(c, req, build)
-		log.Infof(c, "determined subsystems: %q", newSubsystems)
 		if err := saveCrash(c, ns, req, bug, bugKey, build, assets); err != nil {
 			return nil, err
 		}
 	} else {
 		log.Infof(c, "not saving crash for %q", bug.Title)
 	}
+
+	newSubsystems := []*subsystem.Subsystem{}
+	// Recalculate subsystems on the first saved crash and on the first saved repro.
+	calculateSubsystems := save && (bug.NumCrashes == 0 ||
+		bug.ReproLevel == ReproLevelNone && reproLevel != ReproLevelNone)
+	if calculateSubsystems {
+		newSubsystems, err = inferSubsystems(c, bug, bugKey)
+		if err != nil {
+			log.Errorf(c, "%q: failed to extract subsystems: %s", bug.Title, err)
+			return nil, err
+		}
+	}
+
 	tx := func(c context.Context) error {
 		bug = new(Bug)
 		if err := db.Get(c, bugKey, bug); err != nil {
@@ -786,8 +796,8 @@ func reportCrash(c context.Context, build *Build, req *dashapi.Crash) (*Bug, err
 		if len(req.Report) != 0 {
 			bug.HasReport = true
 		}
-		for _, name := range newSubsystems {
-			bug.addSubsystem(BugSubsystem{name})
+		if calculateSubsystems {
+			bug.SetSubsystems(newSubsystems)
 		}
 		bug.increaseCrashStats(now)
 		bug.HappenedOn = mergeString(bug.HappenedOn, build.Manager)
@@ -819,28 +829,6 @@ func parseCrashAssets(c context.Context, req *dashapi.Crash) ([]Asset, error) {
 		assets = append(assets, newAsset)
 	}
 	return assets, nil
-}
-
-const overrideSubsystemsKey = "set_subsystems"
-
-func detectCrashSubsystems(c context.Context, req *dashapi.Crash, build *Build) []string {
-	val, ok := c.Value(overrideSubsystemsKey).([]string)
-	if ok {
-		return val
-	}
-	if build.OS == targets.Linux {
-		extractor := subsystem.MakeLinuxSubsystemExtractor()
-		return extractor.Extract(&subsystem.Crash{
-			GuiltyFiles: req.GuiltyFiles,
-			SyzRepro:    string(req.ReproSyz),
-		})
-	}
-	return nil
-}
-
-func subsystemMaintainers(ns, subsystemName string) []string {
-	nsConfig := config.Namespaces[ns]
-	return nsConfig.Subsystems.SubsystemCc(subsystemName)
 }
 
 func (crash *Crash) UpdateReportingPriority(build *Build, bug *Bug) {

dashboard/app/app_test.go
@@ -15,7 +15,8 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/syzkaller/dashboard/dashapi"
 	"github.com/google/syzkaller/pkg/auth"
-	subsystem "github.com/google/syzkaller/pkg/subsystem/legacy"
+	"github.com/google/syzkaller/pkg/subsystem"
+	_ "github.com/google/syzkaller/pkg/subsystem/lists"
 	"github.com/google/syzkaller/sys/targets"
 	"google.golang.org/appengine/v2/user"
 )
@@ -105,6 +106,22 @@ var testConfig = &GlobalConfig{
 						Index: 2,
 					},
 				},
+			},
+			Subsystems: SubsystemsConfig{
+				Service: subsystem.MustMakeService([]*subsystem.Subsystem{
+					{
+						Name:        "subsystemA",
+						PathRules:   []subsystem.PathRule{{IncludeRegexp: `a\.c`}},
+						Lists:       []string{"subsystemA@list.com"},
+						Maintainers: []string{"subsystemA@person.com"},
+					},
+					{
+						Name:        "subsystemB",
+						PathRules:   []subsystem.PathRule{{IncludeRegexp: `b\.c`}},
+						Lists:       []string{"subsystemB@list.com"},
+						Maintainers: []string{"subsystemB@person.com"},
+					},
+				}),
 			},
 		},
 		"test2": {
@@ -358,7 +375,7 @@ var testConfig = &GlobalConfig{
 				},
 			},
 			Subsystems: SubsystemsConfig{
-				SubsystemCc: subsystem.LinuxGetMaintainers,
+				Service: subsystem.ListService("linux"),
 			},
 		},
 		"test-decommission": {

dashboard/app/entities.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/google/syzkaller/dashboard/dashapi"
 	"github.com/google/syzkaller/pkg/hash"
+	"github.com/google/syzkaller/pkg/subsystem"
 	"golang.org/x/net/context"
 	db "google.golang.org/appengine/v2/datastore"
 )
@@ -129,13 +130,11 @@ type BugSubsystem struct {
 	Name string
 }
 
-func (bug *Bug) addSubsystem(subsystem BugSubsystem) {
-	for _, item := range bug.Tags.Subsystems {
-		if item.Name == subsystem.Name {
-			return
-		}
+func (bug *Bug) SetSubsystems(list []*subsystem.Subsystem) {
+	bug.Tags.Subsystems = nil
+	for _, item := range list {
+		bug.Tags.Subsystems = append(bug.Tags.Subsystems, BugSubsystem{Name: item.Name})
 	}
-	bug.Tags.Subsystems = append(bug.Tags.Subsystems, subsystem)
 }
 
 func (bug *Bug) hasSubsystem(name string) bool {