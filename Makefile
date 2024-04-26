anonimiPsyz/Psyzkaller

commit 9180dd629956fbf21bbbedb1e613fb5a17441389

Makefile: build target binaries into separate dirs
We currently build binaries for all targets into bin.
This makes mess in bin/ and does not allow testing of different archs.
Build target binaries into bin/OS_ARCH/ subdirs.

Host binaries are still built into bin/.

Update #333
Update #324
Update #191

Makefile
@@ -1,19 +1,80 @@
 # Copyright 2017 syzkaller project authors. All rights reserved.
 # Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-NOSTATIC ?= 0
-ifeq ($(NOSTATIC), 0)
-	STATIC_FLAG=-static
+# There are 4 OS/arch pairs:
+#  - BUILDOS/BUILDARCH: the current machine's pair used for build.
+#  - HOSTOS/HOSTARCH: pair where syz-manager will run.
+#  - TARGETOS/TARGETVMARCH: pair of the target OS under test.
+#  - TARGETOS/TARGETARCH: pair of the target test process.
+#
+# The last 2 differ for e.g. amd64 OS and 386 test processes (compat syscall testing).
+# All pairs default to the current machine. All but BUILD can be overriden.
+#
+# For example, to test linux/amd64 on linux/amd64, you just run:
+#    make
+# To test linux/arm64 from darwin/amd64 host, run:
+#    make HOSTOS=darwin HOSTARCH=amd64 TARGETOS=linux TARGETARCH=arm64
+# To test x86 compat syscalls, run:
+#    make TARGETVMARCH=amd64 TARGETARCH=386
+#
+# There is a special case for Android builds:
+#    NDK=/path/to/android/ndk make TARGETOS=android TARGETARCH=arm64
+# But you still need to specify "target": "linux/arm64" in syz-manager config.
+
+BUILDOS := $(shell go env GOOS)
+BUILDARCH := $(shell go env GOARCH)
+HOSTOS ?= $(BUILDOS)
+HOSTARCH ?= $(BUILDARCH)
+TARGETOS ?= $(HOSTOS)
+TARGETARCH ?= $(HOSTARCH)
+TARGETVMARCH ?= $(TARGETARCH)
+
+ifeq ("$(TARGETARCH)", "amd64")
+	CC = "x86_64-linux-gnu-gcc"
+else ifeq ("$(TARGETARCH)", "386")
+	CC = "x86_64-linux-gnu-gcc"
+	ADDCFLAGS = "-m32"
+else ifeq ("$(TARGETARCH)", "arm64")
+	CC = "aarch64-linux-gnu-gcc"
+else ifeq ("$(TARGETARCH)", "arm")
+	CC = "arm-linux-gnueabihf-gcc"
+	ADDCFLAGS = "-march=armv6t2"
+else ifeq ("$(TARGETARCH)", "ppc64le")
+	CC = "powerpc64le-linux-gnu-gcc"
 endif
 
-.PHONY: all main tools \
-	manager fuzzer executor \
-	ci hub \
-	execprog mutate prog2c stress repro upgrade db \
-	bin/syz-sysgen bin/syz-extract bin/syz-fmt \
-	extract generate \
-	android \
-	format tidy test arch cross-compile presubmit clean
+ifeq ("$(TARGETOS)", "android")
+	override TARGETOS = "linux"
+	ANDROID_API = 24
+	BUILDGCCARCH = ""
+	ANDROIDARCH = ""
+	TOOLCHAIN = ""
+	GCCBIN = ""
+	ifeq ("$(TARGETARCH)", "amd64")
+		ANDROIDARCH = "x86_64"
+		TOOLCHAIN = "x86_64-4.9"
+		GCCBIN = "x86_64-linux-android-g++"
+	else ifeq ("$(TARGETARCH)", "386")
+		ANDROIDARCH = "x86"
+		TOOLCHAIN = "x86-4.9"
+		GCCBIN = "i686-linux-android-g++"
+	else ifeq ("$(TARGETARCH)", "arm64")
+		ANDROIDARCH = "arm64"
+		TOOLCHAIN = "aarch64-linux-android-4.9"
+		GCCBIN = "aarch64-linux-android-g++"
+	else ifeq ("$(TARGETARCH)", "arm")
+		ANDROIDARCH = "arm"
+		TOOLCHAIN = "arm-linux-androideabi-4.9"
+		GCCBIN = "arm-linux-androideabi-g++"
+	endif
+	ifeq ("$(BUILDARCH)", "amd64")
+		BUILDGCCARCH = "x86_64"
+	else ifeq ("$(BUILDARCH)", "arm64")
+		BUILDGCCARCH = "aarch64"
+	endif
+	CC = $(NDK)/toolchains/$(TOOLCHAIN)/prebuilt/$(BUILDOS)-$(BUILDGCCARCH)/bin/$(GCCBIN)
+	CFLAGS = -I $(NDK)/sources/cxx-stl/llvm-libc++/include --sysroot=$(NDK)/platforms/android-$(ANDROID_API)/arch-$(ANDROIDARCH) -O1 -g -Wall -static
+endif
 
 GITREV=$(shell git rev-parse HEAD)
 ifeq ($(`git diff --shortstat`), "")
@@ -22,37 +83,54 @@ else
 	REV=$(GITREV)+
 endif
 
-all:
-	$(MAKE) main
-	$(MAKE) tools
-
-main:
-	go install ./syz-manager ./syz-fuzzer
-	$(MAKE) manager
-	$(MAKE) fuzzer
-	$(MAKE) executor
-
-tools: execprog mutate prog2c stress repro upgrade db
-
-# executor uses stacks of limited size, so no jumbo frames.
-executor:
-	$(CC) -o ./bin/syz-executor executor/executor.cc -pthread -Wall -Wframe-larger-than=8192 \
-		-Wparentheses -Werror -O1 -g $(STATIC_FLAG) $(CFLAGS) -DGIT_REVISION=\"$(REV)\"
+NOSTATIC ?= 0
+ifeq ($(NOSTATIC), 0)
+	ADDCFLAGS += -static
+endif
 
 # Don't generate symbol table and DWARF debug info.
 # Reduces build time and binary sizes considerably.
 # That's only needed if you use gdb or nm.
 # If you need that, build manually without these flags.
-GOFLAGS="-ldflags=-s -w -X github.com/google/syzkaller/sys.GitRevision=$(REV)"
+GOFLAGS := "-ldflags=-s -w -X github.com/google/syzkaller/sys.GitRevision=$(REV)"
+ifneq ("$(GOTAGS)", "")
+	GOFLAGS += "-tags=$(GOTAGS)"
+endif
+
+.PHONY: all host target \
+	manager fuzzer executor \
+	ci hub \
+	execprog mutate prog2c stress repro upgrade db \
+	bin/syz-sysgen bin/syz-extract bin/syz-fmt \
+	extract generate \
+	format tidy test arch presubmit clean
+
+all: host target
+
+host:
+	GOOS=$(HOSTOS) GOARCH=$(HOSTARCH) go install ./syz-manager
+	env GOOS=$(HOSTOS) GOARCH=$(HOSTARCH) $(MAKE) manager repro mutate prog2c db upgrade
+
+target:
+	GOOS=$(TARGETOS) GOARCH=$(TARGETVMARCH) go install ./syz-fuzzer
+	env GOOS=$(TARGETOS) GOARCH=$(TARGETVMARCH) $(MAKE) fuzzer execprog stress
+	$(MAKE) executor
+
+# executor uses stacks of limited size, so no jumbo frames.
+executor:
+	mkdir -p ./bin/$(TARGETOS)_$(TARGETARCH)
+	$(CC) -o ./bin/$(TARGETOS)_$(TARGETARCH)/syz-executor executor/executor.cc \
+		-pthread -Wall -Wframe-larger-than=8192 -Wparentheses -Werror -O1 -g \
+		$(ADDCFLAGS) $(CFLAGS) -DGIT_REVISION=\"$(REV)\"
 
 manager:
 	go build $(GOFLAGS) -o ./bin/syz-manager github.com/google/syzkaller/syz-manager
 
 fuzzer:
-	go build $(GOFLAGS) -o ./bin/syz-fuzzer github.com/google/syzkaller/syz-fuzzer
+	go build $(GOFLAGS) -o ./bin/$(TARGETOS)_$(TARGETVMARCH)/syz-fuzzer github.com/google/syzkaller/syz-fuzzer
 
 execprog:
-	go build $(GOFLAGS) -o ./bin/syz-execprog github.com/google/syzkaller/tools/syz-execprog
+	go build $(GOFLAGS) -o ./bin/$(TARGETOS)_$(TARGETVMARCH)/syz-execprog github.com/google/syzkaller/tools/syz-execprog
 
 ci:
 	go build $(GOFLAGS) -o ./bin/syz-ci github.com/google/syzkaller/syz-ci
@@ -70,7 +148,7 @@ prog2c:
 	go build $(GOFLAGS) -o ./bin/syz-prog2c github.com/google/syzkaller/tools/syz-prog2c
 
 stress:
-	go build $(GOFLAGS) -o ./bin/syz-stress github.com/google/syzkaller/tools/syz-stress
+	go build $(GOFLAGS) -o ./bin/$(TARGETOS)_$(TARGETVMARCH)/syz-stress github.com/google/syzkaller/tools/syz-stress
 
 db:
 	go build $(GOFLAGS) -o ./bin/syz-db github.com/google/syzkaller/tools/syz-db
@@ -93,7 +171,7 @@ bin/syz-sysgen:
 format: bin/syz-fmt
 	go fmt ./...
 	clang-format --style=file -i executor/*.cc executor/*.h tools/kcovtrace/*.c
-	bin/syz-fmt sys
+	bin/syz-fmt sys/linux
 bin/syz-fmt:
 	go build $(GOFLAGS) -o $@ ./tools/syz-fmt
 
@@ -108,12 +186,15 @@ test:
 	go test -short -race ./...
 
 arch:
-	GOOS=linux GOARCH=amd64 go install ./...
-	GOOS=linux GOARCH=arm64 go install ./...
-	GOOS=linux GOARCH=386 go install ./...
-	GOOS=linux GOARCH=arm go install ./...
-	GOOS=linux GOARCH=ppc64le go install ./...
-	GOOS=darwin GOARCH=amd64 go build -o /dev/null ./syz-manager
+	env HOSTOS=darwin HOSTARCH=amd64 $(MAKE) host
+	env HOSTOS=linux HOSTARCH=amd64 $(MAKE) host
+	env TARGETOS=linux TARGETARCH=amd64 $(MAKE) target
+	env TARGETOS=linux TARGETARCH=386 $(MAKE) target
+	env TARGETOS=linux TARGETARCH=arm64 $(MAKE) target
+	env TARGETOS=linux TARGETARCH=ppc64le $(MAKE) target
+	# executor build on arm fails with:
+	# Error: alignment too large: 15 assumed
+	env TARGETOS=linux TARGETARCH=arm64 TARGETVMARCH=arm $(MAKE) target
 
 presubmit:
 	$(MAKE) generate
@@ -124,20 +205,3 @@ presubmit:
 
 clean:
 	rm -rf ./bin/
-
-cross-compile:
-	# We could use arm-linux-gnueabihf-gcc from  g++-arm-linux-gnueabihf package,
-	# but it is broken with "Error: alignment too large: 15 assumed"
-	env CC="clang" CFLAGS="--target=linux-armv6 -mfloat-abi=hard" $(MAKE) executor
-
-android: UNAME=$(shell uname | tr '[:upper:]' '[:lower:]')
-android: ANDROID_ARCH=arm64
-android: ANDROID_API=24
-android: TOOLCHAIN=aarch64-linux-android
-android:
-	test -d $(NDK)
-	$(MAKE) manager
-	env GOOS=linux GOARCH=arm64 $(MAKE) execprog fuzzer
-	env CC="$(NDK)/toolchains/$(TOOLCHAIN)-4.9/prebuilt/$(UNAME)-x86_64/bin/$(TOOLCHAIN)-g++" \
-		CFLAGS="-I $(NDK)/sources/cxx-stl/llvm-libc++/include --sysroot=$(NDK)/platforms/android-$(ANDROID_API)/arch-$(ANDROID_ARCH) -O1 -g -Wall -static" \
-		$(MAKE) executor

.travis.yml
@@ -17,7 +17,7 @@ before_install:
   - sudo cp clang+llvm-4.0.1-x86_64-linux-gnu-debian8/bin/clang-format /usr/local/bin/
   - which clang-format
   - clang-format --version
-  - sudo apt-get install -y -q libc6-dev-i386 gcc-aarch64-linux-gnu gcc-powerpc64le-linux-gnu gcc-arm-linux-gnueabihf
+  - sudo apt-get install -y -q libc6-dev-i386 linux-libc-dev g++-aarch64-linux-gnu g++-powerpc64le-linux-gnu g++-arm-linux-gnueabihf
 
 install: true
 

syz-ci/syzupdater.go
@@ -8,28 +8,21 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"syscall"
 	"time"
 
+	"github.com/google/syzkaller/pkg/config"
 	"github.com/google/syzkaller/pkg/git"
 	. "github.com/google/syzkaller/pkg/log"
 	"github.com/google/syzkaller/pkg/osutil"
+	"github.com/google/syzkaller/syz-manager/mgrconfig"
 )
 
 const (
 	syzkallerRebuildPeriod = 12 * time.Hour
 	buildRetryPeriod       = 15 * time.Minute // used for both syzkaller and kernel
 )
-
-// List of required files in syzkaller build (contents of latest/current dirs).
-var syzFiles = []string{
-	"tag",        // contains syzkaller repo git hash
-	"bin/syz-ci", // these are just copied from syzkaller dir
-	"bin/syz-manager",
-	"bin/syz-fuzzer",
-	"bin/syz-executor",
-	"bin/syz-execprog",
-}
 
 // SyzUpdater handles everything related to syzkaller updates.
 // As kernel builder, it maintains 2 builds:
@@ -45,6 +38,8 @@ type SyzUpdater struct {
 	syzkallerDir string
 	latestDir    string
 	currentDir   string
+	syzFiles     []string
+	targets      map[string]bool
 }
 
 func NewSyzUpdater(cfg *Config) *SyzUpdater {
@@ -70,6 +65,32 @@ func NewSyzUpdater(cfg *Config) *SyzUpdater {
 	syzkallerDir := filepath.Join(gopath, "src", "github.com", "google", "syzkaller")
 	osutil.MkdirAll(syzkallerDir)
 
+	// List of required files in syzkaller build (contents of latest/current dirs).
+	files := map[string]bool{
+		"tag":             true, // contains syzkaller repo git hash
+		"bin/syz-ci":      true, // these are just copied from syzkaller dir
+		"bin/syz-manager": true,
+	}
+	targets := make(map[string]bool)
+	for _, mgr := range cfg.Managers {
+		mgrcfg := new(mgrconfig.Config)
+		if err := config.LoadData(mgr.Manager_Config, mgrcfg); err != nil {
+			Fatalf("failed to load manager %v config: %v", mgr.Name, err)
+		}
+		os, vmarch, arch, err := mgrconfig.SplitTarget(mgrcfg.Target)
+		if err != nil {
+			Fatalf("failed to load manager %v config: %v", mgr.Name, err)
+		}
+		targets[os+"/"+vmarch+"/"+arch] = true
+		files[fmt.Sprintf("bin/%v_%v/syz-fuzzer", os, vmarch)] = true
+		files[fmt.Sprintf("bin/%v_%v/syz-execprog", os, vmarch)] = true
+		files[fmt.Sprintf("bin/%v_%v/syz-executor", os, arch)] = true
+	}
+	var syzFiles []string
+	for f := range files {
+		syzFiles = append(syzFiles, f)
+	}
+
 	return &SyzUpdater{
 		exe:          exe,
 		repo:         cfg.Syzkaller_Repo,
@@ -78,6 +99,8 @@ func NewSyzUpdater(cfg *Config) *SyzUpdater {
 		syzkallerDir: syzkallerDir,
 		latestDir:    filepath.Join("syzkaller", "latest"),
 		currentDir:   filepath.Join("syzkaller", "current"),
+		syzFiles:     syzFiles,
+		targets:      targets,
 	}
 }
 
@@ -91,7 +114,7 @@ func (upd *SyzUpdater) UpdateOnStart(shutdown chan struct{}) {
 	if exeTag == latestTag && time.Since(exeMod) < syzkallerRebuildPeriod/2 {
 		// Have a freash up-to-date build, probably just restarted.
 		Logf(0, "current executable is up-to-date (%v)", exeTag)
-		if err := osutil.LinkFiles(upd.latestDir, upd.currentDir, syzFiles); err != nil {
+		if err := osutil.LinkFiles(upd.latestDir, upd.currentDir, upd.syzFiles); err != nil {
 			Fatal(err)
 		}
 		return
@@ -112,7 +135,7 @@ func (upd *SyzUpdater) UpdateOnStart(shutdown chan struct{}) {
 			// The build was successful or we had the latest build from previous runs.
 			// Either way, use the latest build.
 			Logf(0, "using syzkaller built on %v", latestTag)
-			if err := osutil.LinkFiles(upd.latestDir, upd.currentDir, syzFiles); err != nil {
+			if err := osutil.LinkFiles(upd.latestDir, upd.currentDir, upd.syzFiles); err != nil {
 				Fatal(err)
 			}
 			if exeTag != latestTag {
@@ -203,8 +226,19 @@ func (upd *SyzUpdater) build() error {
 	if _, err := osutil.RunCmd(time.Hour, upd.syzkallerDir, "make", "generate"); err != nil {
 		return fmt.Errorf("build failed: %v", err)
 	}
-	if _, err := osutil.RunCmd(time.Hour, upd.syzkallerDir, "make", "all", "ci"); err != nil {
+	if _, err := osutil.RunCmd(time.Hour, upd.syzkallerDir, "make", "host", "ci"); err != nil {
 		return fmt.Errorf("build failed: %v", err)
+	}
+	for target := range upd.targets {
+		parts := strings.Split(target, "/")
+		env := []string{
+			"TARGETOS=" + parts[0],
+			"TARGETVMARCH=" + parts[1],
+			"TARGETARCH=" + parts[2],
+		}
+		if _, err := osutil.RunCmdEnv(time.Hour, env, upd.syzkallerDir, "make", "target"); err != nil {
+			return fmt.Errorf("build failed: %v", err)
+		}
 	}
 	if _, err := osutil.RunCmd(time.Hour, upd.syzkallerDir, "go", "test", "-short", "./..."); err != nil {
 		return fmt.Errorf("tests failed: %v", err)
@@ -213,7 +247,7 @@ func (upd *SyzUpdater) build() error {
 	if err := osutil.WriteFile(tagFile, []byte(commit)); err != nil {
 		return fmt.Errorf("filed to write tag file: %v", err)
 	}
-	if err := osutil.CopyFiles(upd.syzkallerDir, upd.latestDir, syzFiles); err != nil {
+	if err := osutil.CopyFiles(upd.syzkallerDir, upd.latestDir, upd.syzFiles); err != nil {
 		return fmt.Errorf("filed to copy syzkaller: %v", err)
 	}
 	return nil
@@ -222,7 +256,7 @@ func (upd *SyzUpdater) build() error {
 // checkLatest returns tag of the latest build,
 // or an empty string if latest build is missing/broken.
 func (upd *SyzUpdater) checkLatest() string {
-	if !osutil.FilesExist(upd.latestDir, syzFiles) {
+	if !osutil.FilesExist(upd.latestDir, upd.syzFiles) {
 		return ""
 	}
 	tag, _ := readTag(filepath.Join(upd.latestDir, "tag"))

syz-manager/mgrconfig/mgrconfig.go
@@ -101,23 +101,15 @@ func load(data []byte, filename string) (*Config, error) {
 		}
 	}
 
-	if cfg.Target == "" {
-		return nil, fmt.Errorf("config param target is empty")
-	}
-	targetParts := strings.Split(cfg.Target, "/")
-	if len(targetParts) != 2 && len(targetParts) != 3 {
-		return nil, fmt.Errorf("bad config param target")
-	}
-	cfg.TargetOS = targetParts[0]
-	cfg.TargetVMArch = targetParts[1]
-	cfg.TargetArch = targetParts[1]
-	if len(targetParts) == 3 {
-		cfg.TargetArch = targetParts[2]
+	var err error
+	cfg.TargetOS, cfg.TargetVMArch, cfg.TargetArch, err = SplitTarget(cfg.Target)
+	if err != nil {
+		return nil, err
 	}
 
-	cfg.SyzFuzzerBin = filepath.Join(cfg.Syzkaller, "bin", "syz-fuzzer")
-	cfg.SyzExecprogBin = filepath.Join(cfg.Syzkaller, "bin", "syz-execprog")
-	cfg.SyzExecutorBin = filepath.Join(cfg.Syzkaller, "bin", "syz-executor")
+	cfg.SyzFuzzerBin = filepath.Join(cfg.Syzkaller, "bin", cfg.TargetOS+"_"+cfg.TargetVMArch, "syz-fuzzer")
+	cfg.SyzExecprogBin = filepath.Join(cfg.Syzkaller, "bin", cfg.TargetOS+"_"+cfg.TargetVMArch, "syz-execprog")
+	cfg.SyzExecutorBin = filepath.Join(cfg.Syzkaller, "bin", cfg.TargetOS+"_"+cfg.TargetArch, "syz-executor")
 	if !osutil.IsExist(cfg.SyzFuzzerBin) {
 		return nil, fmt.Errorf("bad config syzkaller param: can't find %v", cfg.SyzFuzzerBin)
 	}
@@ -169,6 +161,23 @@ func load(data []byte, filename string) (*Config, error) {
 	}
 
 	return cfg, nil
+}
+
+func SplitTarget(target string) (string, string, string, error) {
+	if target == "" {
+		return "", "", "", fmt.Errorf("target is empty")
+	}
+	targetParts := strings.Split(target, "/")
+	if len(targetParts) != 2 && len(targetParts) != 3 {
+		return "", "", "", fmt.Errorf("bad config param target")
+	}
+	os := targetParts[0]
+	vmarch := targetParts[1]
+	arch := targetParts[1]
+	if len(targetParts) == 3 {
+		arch = targetParts[2]
+	}
+	return os, vmarch, arch, nil
 }
 
 func ParseEnabledSyscalls(cfg *Config) (map[int]bool, error) {

commit dda3b472a7a14276469c9ee6b6f2b90fc61ef9ee

Makefile: fix cross-arch CI builds
Using env to pass env don't seem to work as demonstrated by #4717.
It looks like it's broken since 9180dd629956.

Makefile
@@ -317,73 +317,73 @@ presubmit_build: descriptions
 	SYZ_SKIP_DEV_APPSERVER_TESTS=1 $(MAKE) test
 
 presubmit_arch_linux: descriptions
-	env HOSTOS=linux HOSTARCH=amd64 $(MAKE) host
-	env TARGETOS=linux TARGETARCH=amd64 $(MAKE) target
-	env TARGETOS=linux TARGETARCH=386 $(MAKE) target
-	env TARGETOS=linux TARGETARCH=arm64 $(MAKE) target
-	env TARGETOS=linux TARGETARCH=arm $(MAKE) target
-	env TARGETOS=linux TARGETARCH=mips64le $(MAKE) target
-	env TARGETOS=linux TARGETARCH=ppc64le $(MAKE) target
-	env TARGETOS=linux TARGETARCH=riscv64 $(MAKE) target
-	env TARGETOS=linux TARGETARCH=s390x $(MAKE) target
+	HOSTOS=linux HOSTARCH=amd64 $(MAKE) host
+	TARGETOS=linux TARGETARCH=amd64 TARGETVMARCH=amd64 $(MAKE) target
+	TARGETOS=linux TARGETARCH=386 TARGETVMARCH=386 $(MAKE) target
+	TARGETOS=linux TARGETARCH=arm64 TARGETVMARCH=arm64 $(MAKE) target
+	TARGETOS=linux TARGETARCH=arm TARGETVMARCH=arm $(MAKE) target
+	TARGETOS=linux TARGETARCH=mips64le TARGETVMARCH=mips64le $(MAKE) target
+	TARGETOS=linux TARGETARCH=ppc64le TARGETVMARCH=ppc64le $(MAKE) target
+	TARGETOS=linux TARGETARCH=riscv64 TARGETVMARCH=riscv64 $(MAKE) target
+	TARGETOS=linux TARGETARCH=s390x TARGETVMARCH=s390x $(MAKE) target
 
 presubmit_arch_freebsd: descriptions
-	env HOSTOS=freebsd HOSTARCH=amd64 $(MAKE) host
-	env TARGETOS=freebsd TARGETARCH=amd64 $(MAKE) target
-	env TARGETOS=freebsd TARGETARCH=386 $(MAKE) target
-	env TARGETOS=freebsd TARGETARCH=arm64 $(MAKE) target
-	env TARGETOS=freebsd TARGETARCH=riscv64 $(MAKE) target
+	HOSTOS=freebsd HOSTARCH=amd64 $(MAKE) host
+	TARGETOS=freebsd TARGETARCH=amd64 TARGETVMARCH=amd64 $(MAKE) target
+	TARGETOS=freebsd TARGETARCH=386 TARGETVMARCH=386 $(MAKE) target
+	TARGETOS=freebsd TARGETARCH=arm64 TARGETVMARCH=arm64 $(MAKE) target
+	TARGETOS=freebsd TARGETARCH=riscv64 TARGETVMARCH=riscv64 $(MAKE) target
 
 presubmit_arch_netbsd: descriptions
-	env HOSTOS=netbsd HOSTARCH=amd64 $(MAKE) host
-	env TARGETOS=netbsd TARGETARCH=amd64 $(MAKE) target
+	HOSTOS=netbsd HOSTARCH=amd64 $(MAKE) host
+	TARGETOS=netbsd TARGETARCH=amd64 TARGETVMARCH=amd64 $(MAKE) target
 
 presubmit_arch_openbsd: descriptions
-	env HOSTOS=openbsd HOSTARCH=amd64 $(MAKE) host
-	env TARGETOS=openbsd TARGETARCH=amd64 $(MAKE) target
+	HOSTOS=openbsd HOSTARCH=amd64 $(MAKE) host
+	TARGETOS=openbsd TARGETARCH=amd64 TARGETVMARCH=amd64 $(MAKE) target
 
 presubmit_arch_darwin: descriptions
-	env HOSTOS=darwin HOSTARCH=amd64 $(MAKE) host
+	HOSTOS=darwin HOSTARCH=amd64 $(MAKE) host
 
 presubmit_arch_windows: descriptions
-	env TARGETOS=windows TARGETARCH=amd64 $(MAKE) target
+	TARGETOS=windows TARGETARCH=amd64 TARGETVMARCH=amd64 $(MAKE) target
 
 presubmit_arch_executor: descriptions
-	env TARGETOS=linux TARGETARCH=amd64 SYZ_CLANG=yes $(MAKE) executor
-	env TARGETOS=fuchsia TARGETARCH=amd64 $(MAKE) executor
-	env TARGETOS=fuchsia TARGETARCH=arm64 $(MAKE) executor
-	env TARGETOS=test TARGETARCH=64 $(MAKE) executor
-	env TARGETOS=test TARGETARCH=64_fork $(MAKE) executor
-	env TARGETOS=test TARGETARCH=32_shmem $(MAKE) executor
-	env TARGETOS=test TARGETARCH=32_fork_shmem $(MAKE) executor
-	env TARGETOS=test TARGETARCH=64 $(MAKE) executor
-	env TARGETOS=test TARGETARCH=64_fork $(MAKE) executor
-	env TARGETOS=test TARGETARCH=32_shmem $(MAKE) executor
-	env TARGETOS=test TARGETARCH=32_fork_shmem $(MAKE) executor
+	TARGETOS=linux TARGETARCH=amd64 TARGETVMARCH=amd64 SYZ_CLANG=yes $(MAKE) executor
+	TARGETOS=fuchsia TARGETARCH=amd64 TARGETVMARCH=amd64 $(MAKE) executor
+	TARGETOS=fuchsia TARGETARCH=arm64 TARGETVMARCH=arm64 $(MAKE) executor
+	TARGETOS=test TARGETARCH=64 TARGETVMARCH=64 $(MAKE) executor
+	TARGETOS=test TARGETARCH=64_fork TARGETVMARCH=64_fork $(MAKE) executor
+	TARGETOS=test TARGETARCH=32_shmem TARGETVMARCH=32_shmem $(MAKE) executor
+	TARGETOS=test TARGETARCH=32_fork_shmem TARGETVMARCH=32_fork_shmem $(MAKE) executor
+	TARGETOS=test TARGETARCH=64 TARGETVMARCH=64 $(MAKE) executor
+	TARGETOS=test TARGETARCH=64_fork TARGETVMARCH=64_fork $(MAKE) executor
+	TARGETOS=test TARGETARCH=32_shmem TARGETVMARCH=32_shmem $(MAKE) executor
+	TARGETOS=test TARGETARCH=32_fork_shmem TARGETVMARCH=32_fork_shmem $(MAKE) executor
 
 presubmit_dashboard: descriptions
 	SYZ_CLANG=yes $(GO) test -short -vet=off -coverprofile=.coverage.txt ./dashboard/app
 
 presubmit_race: descriptions
 	# -race requires cgo
-	env CGO_ENABLED=1 $(GO) test -race; if test $$? -ne 2; then \
-	env CGO_ENABLED=1 SYZ_SKIP_DEV_APPSERVER_TESTS=1 $(GO) test -race -short -vet=off -bench=.* -benchtime=.2s ./... ;\
+	CGO_ENABLED=1 $(GO) test -race; if test $$? -ne 2; then \
+	CGO_ENABLED=1 SYZ_SKIP_DEV_APPSERVER_TESTS=1 $(GO) test -race -short -vet=off -bench=.* -benchtime=.2s ./... ;\
 	fi
 
 presubmit_race_dashboard: descriptions
 	# -race requires cgo
-	env CGO_ENABLED=1 $(GO) test -race; if test $$? -ne 2; then \
-	env CGO_ENABLED=1 $(GO) test -race -short -vet=off -bench=.* -benchtime=.2s ./dashboard/app/... ;\
+	CGO_ENABLED=1 $(GO) test -race; if test $$? -ne 2; then \
+	CGO_ENABLED=1 $(GO) test -race -short -vet=off -bench=.* -benchtime=.2s ./dashboard/app/... ;\
 	fi
 
 presubmit_old: descriptions
 	# Binaries we can compile in syz-old-env. 386 is broken, riscv64 is missing.
-	TARGETARCH=amd64 $(MAKE) target
-	TARGETARCH=arm64 $(MAKE) target
-	TARGETARCH=arm $(MAKE) target
-	TARGETARCH=ppc64le $(MAKE) target
-	TARGETARCH=mips64le $(MAKE) target
-	TARGETARCH=s390x $(MAKE) target
+	TARGETARCH=amd64 TARGETVMARCH=amd64 $(MAKE) target
+	TARGETARCH=arm64 TARGETVMARCH=arm64 $(MAKE) target
+	TARGETARCH=arm TARGETVMARCH=arm $(MAKE) target
+	TARGETARCH=ppc64le TARGETVMARCH=ppc64le $(MAKE) target
+	TARGETARCH=mips64le TARGETVMARCH=mips64le $(MAKE) target
+	TARGETARCH=s390x TARGETVMARCH=s390x $(MAKE) target
 
 test: descriptions
 	$(GO) test -short -coverprofile=.coverage.txt ./...