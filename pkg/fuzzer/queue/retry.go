a-nogikh/syzkaller

pkg/fuzzer: introduce a request restarter layer
Make Result statuses more elaborate.
Instead of retrying inputs directly in rpc.go, extract this logic to a
separate entity in pkg/fuzzer/queue.

pkg/fuzzer/queue/retry.go
@@ -1,0 +1,37 @@
+// Copyright 2024 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package queue
+
+type retryer struct {
+	pq   *PlainQueue
+	base Source
+}
+
+// NewRetryer adds a layer that resends results with Status=Restarted.
+func NewRetryer(base Source) Source {
+	return &retryer{
+		base: base,
+		pq:   Plain(),
+	}
+}
+
+func (r *retryer) Next() *Request {
+	req := r.pq.tryNext()
+	if req == nil {
+		req = r.base.Next()
+	}
+	if req != nil {
+		req.OnDone(r.done)
+	}
+	return req
+}
+
+func (r *retryer) done(req *Request, res *Result) bool {
+	// The input was on a restarted VM.
+	if res.Status == Restarted {
+		r.pq.Submit(req)
+		return false
+	}
+	return true
+}

syz-manager/manager.go
@@ -25,6 +25,7 @@ import (
 	"github.com/google/syzkaller/pkg/csource"
 	"github.com/google/syzkaller/pkg/db"
 	"github.com/google/syzkaller/pkg/fuzzer"
+	"github.com/google/syzkaller/pkg/fuzzer/queue"
 	"github.com/google/syzkaller/pkg/gce"
 	"github.com/google/syzkaller/pkg/hash"
 	"github.com/google/syzkaller/pkg/host"
@@ -72,6 +73,7 @@ type Manager struct {
 
 	mu                    sync.Mutex
 	fuzzer                atomic.Pointer[fuzzer.Fuzzer]
+	execSource            atomic.Value // queue.Source
 	phase                 int
 	targetEnabledSyscalls map[*prog.Syscall]bool
 
@@ -1373,6 +1375,7 @@ func (mgr *Manager) machineChecked(features *host.Features, enabledSyscalls map[
 		},
 	}, rnd, mgr.target)
 	mgr.fuzzer.Store(fuzzerObj)
+	mgr.execSource.Store(queue.NewRetryer(fuzzerObj))
 
 	mgr.loadCorpus()
 	mgr.firstConnect.Store(time.Now().Unix())
@@ -1395,8 +1398,8 @@ func (mgr *Manager) corpusMinimization() {
 }
 
 // We need this method since we're not supposed to access Manager fields from RPCServer.
-func (mgr *Manager) getFuzzer() *fuzzer.Fuzzer {
-	return mgr.fuzzer.Load()
+func (mgr *Manager) getExecSource() queue.Source {
+	return mgr.execSource.Load().(queue.Source)
 }
 
 func (mgr *Manager) fuzzerSignalRotation() {

anonimiPsyz/Psyzkaller

pkg/fuzzer/queue/retry.go
@@ -1,0 +1,37 @@
+// Copyright 2024 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package queue
+
+type retryer struct {
+	pq   *PlainQueue
+	base Source
+}
+
+// Retry adds a layer that resends results with Status=Restarted.
+func Retry(base Source) Source {
+	return &retryer{
+		base: base,
+		pq:   Plain(),
+	}
+}
+
+func (r *retryer) Next() *Request {
+	req := r.pq.tryNext()
+	if req == nil {
+		req = r.base.Next()
+	}
+	if req != nil {
+		req.OnDone(r.done)
+	}
+	return req
+}
+
+func (r *retryer) done(req *Request, res *Result) bool {
+	// The input was on a restarted VM.
+	if res.Status == Restarted {
+		r.pq.Submit(req)
+		return false
+	}
+	return true
+}

syz-manager/manager.go
@@ -26,6 +26,7 @@ import (
 	"github.com/google/syzkaller/pkg/db"
 	"github.com/google/syzkaller/pkg/flatrpc"
 	"github.com/google/syzkaller/pkg/fuzzer"
+	"github.com/google/syzkaller/pkg/fuzzer/queue"
 	"github.com/google/syzkaller/pkg/gce"
 	"github.com/google/syzkaller/pkg/hash"
 	"github.com/google/syzkaller/pkg/instance"
@@ -73,6 +74,7 @@ type Manager struct {
 
 	mu                    sync.Mutex
 	fuzzer                atomic.Pointer[fuzzer.Fuzzer]
+	execSource            atomic.Value // queue.Source
 	phase                 int
 	targetEnabledSyscalls map[*prog.Syscall]bool
 
@@ -1374,6 +1376,7 @@ func (mgr *Manager) machineChecked(features flatrpc.Feature, enabledSyscalls map
 		},
 	}, rnd, mgr.target)
 	mgr.fuzzer.Store(fuzzerObj)
+	mgr.execSource.Store(queue.Retry(fuzzerObj))
 
 	mgr.loadCorpus()
 	mgr.firstConnect.Store(time.Now().Unix())
@@ -1396,8 +1399,8 @@ func (mgr *Manager) corpusMinimization() {
 }
 
 // We need this method since we're not supposed to access Manager fields from RPCServer.
-func (mgr *Manager) getFuzzer() *fuzzer.Fuzzer {
-	return mgr.fuzzer.Load()
+func (mgr *Manager) getExecSource() queue.Source {
+	return mgr.execSource.Load().(queue.Source)
 }
 
 func (mgr *Manager) fuzzerSignalRotation() {